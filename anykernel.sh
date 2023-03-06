003b0d624f083b0fc4fa43e49f464d07b4daf16f
TheDoggo <99896701+wusixlHalo@users.noreply.github.com>
2023-03-06 22:12:48 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=IdeaKernel by Dogggee@Coolpak-Developer
+kernel.string=DarwinKernel by Dogggee @ Coolpak-Developer
 do.devicecheck=1
 do.modules=0
 do.systemless=1
