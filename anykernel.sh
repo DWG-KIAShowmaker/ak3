eae4e0193f34eada3c9e11d72b11b2b5541dccd4
TheDoggo <99896701+wusixlHalo@users.noreply.github.com>
2023-05-25 18:44:31 +0800

    更新 anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Darwin Kernel by Amagi_Mata @ Coolapk-Developer
+kernel.string=Lemons Kernel by Amagi_Mata @ Coolapk-Developer
 do.devicecheck=1
 do.modules=0
 do.systemless=1
